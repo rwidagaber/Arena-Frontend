4531ecd2367ac8b49414c55876eb715ed7b5ab60
rwida <rwidajaber@gmail.com>
2026-06-02 12:43:35 +0300

    Project setup

---
+++ ArenaFrontend/src/app/app.config.ts
@@ -0,0 +1,23 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+
+import { routes } from './app.routes';
+import { authInterceptor } from './core/interceptors/auth/auth-interceptor';
+import { errorInterceptor } from './core/interceptors/error/error-interceptor';
+import { loadingInterceptor } from './core/interceptors/loading/loading-interceptor';
+import { loggingInterceptor } from './core/interceptors/logging/logging-interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([
+      authInterceptor,
+      loadingInterceptor,
+      loggingInterceptor,
+      errorInterceptor,
+    ]))
+  ]
+};
---
+++ ArenaFrontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  developerMode: false,
+  // apiUrl: 'https://api.arena.com/api'
+};
