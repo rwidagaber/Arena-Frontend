1bfbfadb0faa1784be7b0e6d033e3993dfa64a08
Hajar Ismail <hajarismail2030@gmail.com>
2026-06-03 01:20:45 +0200

    add login and register guard

--- ArenaFrontend/src/app/app.routes.ts
+++ ArenaFrontend/src/app/app.routes.ts
@@ -2,15 +2,20 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/Authentication/register/register';
 import { LoginComponent } from './features/Authentication/login/login';
 import { Home } from './features/home/home';
+import { guestGuard } from './core/guards/auth/guest-guard-guard';
 
 export const routes: Routes = [
      {
     path: 'login',
-    component: LoginComponent
+    component: LoginComponent,
+        canActivate: [guestGuard]
+      
   },
   {
     path: 'register',
-    component: RegisterComponent
+    component: RegisterComponent,
+        canActivate: [guestGuard]
+
   },
    {
     path: 'home',
--- ArenaFrontend/src/app/core/guards/auth/auth-guard.ts
+++ ArenaFrontend/src/app/core/guards/auth/auth-guard.ts
@@ -2,13 +2,13 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../services/auth';
 
-/** يحمي الـ routes اللي محتاجة login */
 export const authGuard: CanActivateFn = (_route, state) => {
-  const auth   = inject(AuthService);
+  const auth = inject(AuthService);
   const router = inject(Router);
 
   if (auth.isLoggedIn) return true;
 
-  router.navigate(['/'], { queryParams: { returnUrl: state.url } });
-  return false;
+  return router.createUrlTree(['/login'], {
+    queryParams: { returnUrl: state.url }
+  });
 };
