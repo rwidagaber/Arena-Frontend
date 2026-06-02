b1812dccd7096765fe7a83d56e33fc7f1aa1bda4
rwida <rwidajaber@gmail.com>
2026-06-03 00:40:19 +0300

    Handle redirection in home

---
+++ ArenaFrontend/src/app/features/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeroComponent } from "../hero/hero.component";
+import { ProgramsComponent } from "../programs/programs.component";
+import { WhatWeDoComponent } from "../what-we-do/what-we-do.component";
+import { WhyChooseUsComponent } from "../why-choose-us/why-choose-us.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [HeroComponent, ProgramsComponent, WhatWeDoComponent, WhyChooseUsComponent],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
