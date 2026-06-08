975050200f1a1d7cea586bf54a5566e9e6224fe9
Nada Osama <enada1349@gmail.com>
2026-06-08 23:33:16 +0300

    fix: move header to app level, always show subscription pill in dropdown

--- ArenaFrontend/src/app/app.ts
+++ ArenaFrontend/src/app/app.ts
@@ -1,19 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header';
-import { HeroComponent } from './features/hero/hero.component';
-import { ProgramsComponent } from './features/programs/programs.component';
-import { WhatWeDoComponent } from './features/what-we-do/what-we-do.component';
-import { WhyChooseUsComponent } from './features/why-choose-us/why-choose-us.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    HeaderComponent
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('ArenaFrontend');
-}
+export class App {}
