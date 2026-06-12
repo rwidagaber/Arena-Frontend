9fc1b684e56f532bd31b163ef6856bb3beea37cb
rwida <rwidajaber@gmail.com>
2026-06-12 21:25:49 +0300

    Revert "update env"
    This reverts commit 08f4d1abb3d8a52fa738614c19ac9259af4420e9.

--- ArenaFrontend/src/environments/environment.ts
+++ ArenaFrontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   developerMode: false,
-  apiUrl: 'https://arenagym.runasp.net/api'
+  apiUrl: 'http://arenagym.runasp.net/api'
   // 'http://localhost:5095/api'
 };
 
