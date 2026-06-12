08f4d1abb3d8a52fa738614c19ac9259af4420e9
rwida <rwidajaber@gmail.com>
2026-06-12 21:01:05 +0300

    update env

--- ArenaFrontend/src/environments/environment.development.ts
+++ ArenaFrontend/src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   developerMode: true,
   // apiUrl: 'http://localhost:5095/api'
-  apiUrl: 'http://arenagym.runasp.net/api'
+  apiUrl: 'https://arenagym.runasp.net/api'
 };
