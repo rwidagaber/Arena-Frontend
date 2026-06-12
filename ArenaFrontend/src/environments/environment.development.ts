5bf9806e8e2ab3bb3d3a96517b96d6bdf4bb0e91
rwida <rwidajaber@gmail.com>
2026-06-12 16:17:27 +0300

    deploy: fix client side routing fallback for angular app

--- ArenaFrontend/src/environments/environment.development.ts
+++ ArenaFrontend/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   developerMode: true,
-  apiUrl: 'http://localhost:5095/api'
+ apiUrl: 'http://arenagym.runasp.net/api'
+  // 'http://localhost:5095/api'
 };
