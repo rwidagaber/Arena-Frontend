d3509009cdfc60c2f224694ecaa0f1acad42f035
Hajar Ismail <hajarismail2030@gmail.com>
2026-06-23 08:09:46 +0200

    add notification page

--- ArenaFrontend/src/environments/environment.development.ts
+++ ArenaFrontend/src/environments/environment.development.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: false,
   developerMode: true,
-  // apiUrl: 'http://localhost:5095/api'
-  apiUrl: 'http://arenagym.runasp.net/api'
-
-  // test
-
+  apiUrl: 'http://localhost:5095/api',
+  // apiUrl: 'http://arenagym.runasp.net/api',
+   vapidPublicKey: 'BBVVNzENeRxvLf8M5dNVDd3jt63Cl5HyKff1HZrQRku22k0nuVom-kdl5CSYH1DlvnoeRDo6Hevtj_47ikY0YT8'
 };
