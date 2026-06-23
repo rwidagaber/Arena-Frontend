7586f5839ba93ca16bce91ef4f361a141c2612de
Hajar Ismail <hajarismail2030@gmail.com>
2026-06-23 10:42:13 +0200

    Revert "Merge pull request #89 from rwidagaber/feature/signalR"
    This reverts commit 6f38a10c47ae2d95451cc1d0108e2a24a19ef36d, reversing
    changes made to 714a77883ae1ace76b314b24bc713f91865b2cb1.

--- ArenaFrontend/src/environments/environment.development.ts
+++ ArenaFrontend/src/environments/environment.development.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
   developerMode: true,
-  // apiUrl: 'http://localhost:5095/api',
-  apiUrl: 'http://arenagym.runasp.net/api',
-   vapidPublicKey: 'BBVVNzENeRxvLf8M5dNVDd3jt63Cl5HyKff1HZrQRku22k0nuVom-kdl5CSYH1DlvnoeRDo6Hevtj_47ikY0YT8'
+  // apiUrl: 'http://localhost:5095/api'
+  apiUrl: 'http://arenagym.runasp.net/api'
+
+  // test
+
 };
--- ArenaFrontend/src/environments/environment.ts
+++ ArenaFrontend/src/environments/environment.ts
@@ -2,8 +2,7 @@ export const environment = {
   production: true, 
   developerMode: false,
 
-  apiUrl: 'http://arenagym.runasp.net/api',
-  // apiUrl: 'http://localhost:5095/api',
-  vapidPublicKey: 'BBVVNzENeRxvLf8M5dNVDd3jt63Cl5HyKff1HZrQRku22k0nuVom-kdl5CSYH1DlvnoeRDo6Hevtj_47ikY0YT8'
+  apiUrl: 'http://arenagym.runasp.net/api'
+  // apiUrl: 'http://localhost:5095/api'
 };
 
