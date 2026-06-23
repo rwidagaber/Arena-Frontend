002dd3d472870a4ff3832be72cbe5cd3569c4e8c
Hajar Ismail <hajarismail2030@gmail.com>
2026-06-23 11:45:08 +0200

    comment

--- ArenaFrontend/src/environments/environment.ts
+++ ArenaFrontend/src/environments/environment.ts
@@ -5,5 +5,6 @@ export const environment = {
   apiUrl: 'http://arenagym.runasp.net/api',
   // apiUrl: 'http://localhost:5095/api',
   vapidPublicKey: 'BBVVNzENeRxvLf8M5dNVDd3jt63Cl5HyKff1HZrQRku22k0nuVom-kdl5CSYH1DlvnoeRDo6Hevtj_47ikY0YT8'
+  //notification key
 };
 
