fd412a695651054bc4696c83ff6f6313c0198ce6
rwida <rwidajaber@gmail.com>
2026-07-01 17:45:53 +0300

    change environment link

--- ArenaFrontend/src/environments/environment.ts
+++ ArenaFrontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true, 
   developerMode: false,
-  // apiUrl: 'https://arenagym.runasp.net/api',
-  apiUrl: 'http://localhost:5095/api',
+  apiUrl: 'https://arenagym.runasp.net/api',
+  // apiUrl: 'http://localhost:5095/api',
   vapidPublicKey: 'BBVVNzENeRxvLf8M5dNVDd3jt63Cl5HyKff1HZrQRku22k0nuVom-kdl5CSYH1DlvnoeRDo6Hevtj_47ikY0YT8'
 };
 
