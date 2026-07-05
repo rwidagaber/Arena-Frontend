687a20738987c16f3603f0231df7e385c7f0c343
mohammed74eldeeb14 <76451360+mohammed74eldeeb14@users.noreply.github.com>
2026-07-05 18:21:50 +0300

    fix qr scanner

--- ArenaFrontend/src/app/features/QR/qr.service.ts
+++ ArenaFrontend/src/app/features/QR/qr.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { QrDto, QrScanResultDto, ScanQrRequestDto } from './qr.model';
+import { QrDto } from './qr.model';
 
 @Injectable({
   providedIn: 'root',
@@ -14,9 +14,5 @@ export class QrService {
 
   generate(bookingId: string): Observable<QrDto> {
     return this.http.post<QrDto>(`${this.baseUrl}/generate/${bookingId}`, {});
-  }
-
-  scan(dto: ScanQrRequestDto): Observable<QrScanResultDto> {
-    return this.http.post<QrScanResultDto>(`${this.baseUrl}/scan`, dto);
   }
 }
